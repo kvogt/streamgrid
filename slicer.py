9289f05fe0a827bdb47bc2739a071d057903eef3
Kyle Vogt <kyleavogt@gmail.com>
2010-11-12 16:48:37 -0800

    done

--- slicer.py
+++ slicer.py
@@ -1,22 +1,74 @@
 import os
+import sys
 import urllib
+import glob
 import json
 import random
 import traceback
+import pickle
+import random
 from flvtools import flv
 
-data = json.loads(urllib.urlopen("http://usher.justin.tv/find/live_user_officecam.json").read())[0]
-token = data['token']
-host, app = data['connect'].split('/')[2:]
-stream = data['play']
-fname = 'temp_%i.flv' % random.randint(0,100000)
-try:
-	com = "flvpuller.py -h %s -a %s -s %s -m '%s' -o %s" % (host, app, stream, token, fname)
-	output = os.popen(com).read()
-except:
-	traceback.print_exc()
-finally:
-	os.unlink(fname)
-	print "Deleted temporary file %s." % fname
+category = 'gaming'
+limit = 100
+channels = []
+format = 'jpg'
+
+url = "http://usher.justin.tv/stream/list.json?limit=%s" % (limit * 2)
+if category:
+    url += "&category=%s" % category    
+data = json.loads(urllib.urlopen(url).read())
+for row in data:
+    channels.append(row['channel'])
+random.shuffle(channels)
+print "Slicing from following pool of channels (limit %s): %s" % (limit, channels)
+
+seconds = 30.0
+prefix = 'frames/'
+
+completed = 0
+for channel in channels:
+    try:
+        data = json.loads(urllib.urlopen("http://usher.justin.tv/find/live_user_%s.json" % channel).read())[0]
+        token = data['token']
+    except:
+        print "Error slicing channel %s:" % channel
+        traceback.print_exc()
+        continue
+    host, app = data['connect'].split('/')[2:]
+    stream = data['play']
+    fname = 'temp_%i.flv' % random.randint(0,100000)
+    try:
+        print "Capturing %.2f seconds of channel %s..." % (seconds, channel)
+        com = "flvpuller.py -h %s -a %s -s %s -m '%s' -o %s -g %.2f" % (host, app, stream, token, fname, seconds)
+        output = os.popen(com).read()
+        print "Done."
+        print "Examing output file..."
+        size = len(open(fname).read())
+        if size < (seconds * 10000):
+            print "  File too small! (%s bytes)" % size
+            continue
+        print "Cleaning up old frames..."
+        com = "rm %s%s-*.%s" % (prefix, channel, format)
+        output = os.popen(com).read()
+        print "Done."
+        print "Splitting video into individual frames..."
+        com = "ffmpeg -i %s -s 320x240 -f image2 %s%s-%%05d.%s" % (fname, prefix, channel, format)
+        output = os.popen(com).read()
+        print "Done."
+        print "Calculating frame delay..."
+        frames = len(glob.glob('%s%s-*.%s' % (prefix, channel, format)))
+        delay = seconds / frames
+        print "Saved %s frames over %.2fs for a frame delay of %.3f" % (frames, seconds, delay)
+        print "Done."
+        pickle.dump(delay, open(prefix + channel + '.dat', 'w'))
+    except:
+    	traceback.print_exc()
+    finally:
+    	os.unlink(fname)
+    	print "Deleted temporary file %s." % fname
+    completed += 1
+    if completed == limit: 
+        break
 	
 
--- streamgrid.py
+++ streamgrid.py
@@ -1,150 +1,238 @@
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
-import sys, time
+from PIL import Image
+import sys
+import time
+import glob
+import pickle
+import random
 
-# Some api in the chain is translating the keystrokes to this octal string
-# so instead of saying: ESCAPE = 27, we use the following.
-ESCAPE = '\033'
-
-window = 0
 rtri = 0.0
-rquad = 0.0
 counter = 0
-cubelist = None
-
-def InitGL(Width, Height):				# We call this right after our OpenGL window is created.
-    global cubelist
-    glClearColor(0.0, 0.0, 0.0, 0.0)	# This Will Clear The Background Color To Black
-    glClearDepth(1.0)					# Enables Clearing Of The Depth Buffer
-    glDepthFunc(GL_LESS)				# The Type Of Depth Test To Do
-    glEnable(GL_DEPTH_TEST)				# Enables Depth Testing
-    glEnable(GL_TEXTURE_2D)
-    glShadeModel(GL_SMOOTH)				# Enables Smooth Color Shading
-	
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
-    glMatrixMode(GL_MODELVIEW)
-    cubelist = glGenLists(1)
-    glNewList(cubelist, GL_COMPILE)
-    cube(0.3)
-    glEndList()
-
-def ReSizeGLScene(Width, Height):
-    if Height == 0:
-	    Height = 1
-
-    glViewport(0, 0, Width, Height)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
-    glMatrixMode(GL_MODELVIEW)
-
-def cube(c):
-    glBegin(GL_QUADS)			# Start Drawing The Cube
-    glColor3f(0.0,1.0,0.0)			# Set The Color To Blue
-    glVertex3f( c, c,-c)		# Top Right Of The Quad (Top)
-    glVertex3f(-c, c,-c)		# Top Left Of The Quad (Top)
-    glVertex3f(-c, c, c)		# Bottom Left Of The Quad (Top)
-    glVertex3f( c, c, c)		# Bottom Right Of The Quad (Top)
-
-    glColor3f(1.0,0.5,0.0)			# Set The Color To Orange
-    glVertex3f( c,-c, c)		# Top Right Of The Quad (Bottom)
-    glVertex3f(-c,-c, c)		# Top Left Of The Quad (Bottom)
-    glVertex3f(-c,-c,-c)		# Bottom Left Of The Quad (Bottom)
-    glVertex3f( c,-c,-c)		# Bottom Right Of The Quad (Bottom)
-
-    glColor3f(1.0,0.0,0.0)			# Set The Color To Red
-    glVertex3f( c, c, c)		# Top Right Of The Quad (Front)
-    glVertex3f(-c, c, c)		# Top Left Of The Quad (Front)
-    glVertex3f(-c,-c, c)		# Bottom Left Of The Quad (Front)
-    glVertex3f( c,-c, c)		# Bottom Right Of The Quad (Front)
-
-    glColor3f(1.0,1.0,0.0)			# Set The Color To Yellow
-    glVertex3f( c,-c,-c)		# Bottom Left Of The Quad (Back)
-    glVertex3f(-c,-c,-c)		# Bottom Right Of The Quad (Back)
-    glVertex3f(-c, c,-c)		# Top Right Of The Quad (Back)
-    glVertex3f( c, c,-c)		# Top Left Of The Quad (Back)
-
-    glColor3f(0.0,0.0,1.0)			# Set The Color To Blue
-    glVertex3f(-c, c, c)		# Top Right Of The Quad (Left)
-    glVertex3f(-c, c,-c)		# Top Left Of The Quad (Left)
-    glVertex3f(-c,-c,-c)		# Bottom Left Of The Quad (Left)
-    glVertex3f(-c,-c, c)		# Bottom Right Of The Quad (Left)
-
-    glColor3f(1.0,0.0,1.0)			# Set The Color To Violet
-    glVertex3f( c, c,-c)		# Top Right Of The Quad (Right)
-    glVertex3f( c, c, c)		# Top Left Of The Quad (Right)
-    glVertex3f( c,-c, c)		# Bottom Left Of The Quad (Right)
-    glVertex3f( c,-c,-c)		# Bottom Right Of The Quad (Right)
-    glEnd()				# Done Drawing The Quad
-    
-# The main drawing function. 
-def DrawGLScene():
-    global rquad, counter, cubelist
-    
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glLoadIdentity()
-    w = h = 3
-    sc = 1.0
-    dep = 5
-    xoff = 0
-    
-    for x in range(w):
-        for y in range(h):
-            # Evenly spaced, centered grid
-            tx = -(w-1) * sc / 2 + sc * x
-            ty = -(h-1) * sc / 2 + sc * y
-
-            glTranslatef(tx + xoff, ty, -dep)
-            glRotatef(rquad,1.0,1.0,1.0)
-            glCallList(cubelist)
-            glRotatef(-rquad,1.0,1.0,1.0)
-            glTranslatef(-tx - xoff, -ty, dep)
-
-    rquad = rquad - 0.20
-
-    glFinish()
-    glutSwapBuffers()
-
-# The function called whenever a key is pressed. Note the use of Python tuples to pass in: (key, x, y)  
-def keyPressed(*args):
-	# If escape is pressed, kill everything.
-    if args[0] == ESCAPE:
-	    glutDestroyWindow(window)
-	    sys.exit()
-	    
+full = False
+format = 'jpg'
+diffs = {}
+tex_index = {}
 lasttime = time.time()
-def idle():
-    glutPostRedisplay()
 
-def main():
-	global window
+class Channel:
+    
+    def __init__(self, name):
+        self.textures = []
+        self.current_texture = None
+        self.loop_length = 0.0
+        self.loop_delay = 0.03
+        self.last_frame_time = time.time()
+        self.name = name
+        self.width = 0
+        self.height = 0
+        self.prefix = 'frames/'
+        self.window_id = -1
+        self.frame_files = glob.glob(self.prefix + self.name + '*')
+        self.frame_files.sort()
+        try:
+            self.loop_delay = pickle.load(open(self.prefix + self.name + '.dat', 'r'))
+        except:
+            self.loop_delay = 0.03 # 30fps
+        self.frames = len(self.frame_files)
+        self.frame_index = random.randint(1, self.frames) - 1
+        
+    def __repr__(self):
+        return "<Channel '%s'>" % self.name
+        
+    def load_frame(self, frame_id):
+        try:
+            image = Image.open(self.frame_files[frame_id])
+        except:
+            return None
+        self.width, self.height = image.size[0:2]
+        data = image.tostring("raw", "RGBX", 0, -1)
+        return data
 
-	glutInit(sys.argv)
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
-	glutInitWindowSize(640, 480)
-	glutInitWindowPosition(0, 0)
-	window = glutCreateWindow("StreamGrid")
-
-	glutFullScreen()
-	glutDisplayFunc(DrawGLScene)
-	glutIdleFunc(idle)
-	glutReshapeFunc(ReSizeGLScene)
-	glutKeyboardFunc(keyPressed)
-	InitGL(640, 480)
-
+    def load_texture(self, data):
+        tex = glGenTextures(1)
+        self.textures.append(tex)
+        glBindTexture(GL_TEXTURE_2D, tex)
+        glPixelStorei(GL_UNPACK_ALIGNMENT,1)
+        glTexImage2D(GL_TEXTURE_2D, 0, 3, self.width, self.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
+        return tex
+        
+class Stream:
+    
+    def __init__(self, grid):
+        self.grid = grid
+        self.r = 0.0
+        self.dr = random.randint(-30, 30)
+        self.channel = None
+        self.z = 1.0
+        self.dz = 0.0
+        self.initddz = 0.001
+        self.ddz = self.initddz
+        
+    def tick(self, tx, ty, dep, diff):
+        glTranslatef(tx, ty, -dep)
+        # SPIN
+        if self.grid.modes.get('spinning', False):
+            self.r += diff * self.dr
+            glRotatef(self.r,1.0,1.0,1.0)
+        else:
+            self.r = 0.0
+        # ZOOM
+        if self.grid.modes.get('zooming', False):
+            self.z = self.z + self.dz
+            if self.z > 2.0 and self.ddz > 0:
+                self.ddz = -self.ddz
+            if self.z < 0 and self.ddz < 0:
+                self.ddz = -self.ddz
+            self.dz += self.ddz
+            self.dz = min(self.dz, 0.05)
+            self.dz = max(self.dz, -0.05)
+            glTranslatef(0, 0, -(self.z - 1))
+        else:
+            self.z = 1.0
+            self.dz = 0.0
+            self.ddz = self.initddz
+        
+    def untick(self, tx, ty, dep, diff):
+        # UNZOOM
+        if self.grid.modes.get('zooming', False):
+            glTranslatef(0, 0, (self.z - 1))
+        # UNSPIN
+        if self.grid.modes.get('spinning', False):
+            glRotatef(-self.r,1.0,1.0,1.0)
+        glTranslatef(-tx, -ty, dep)
+        
+class StreamGrid:
+    
+    def __init__(self):
+        self.channels = []
+        self.modes = {}
+        self.rquad = 0.0
+        self.grid_w = 6
+        self.grid_h = 4
+        self.last_render = time.time()
+        # Identify available channels
+        prefix = 'frames/'
+        all_files = glob.glob('%s*.%s' % (prefix, format))
+        for name in set([f.split('-')[0].replace(prefix, '') for f in all_files]):
+            chan = Channel(name)
+            self.channels.append(chan)
+            random.shuffle(self.channels)
+        self.streams = [Stream(self) for s in range(self.grid_w * self.grid_h)]
+        for s in self.streams:
+            s.channel = random.choice(self.channels)
+        
+    def init_gl(self, w, h):
+        glClearColor(0.0, 0.0, 0.0, 0.0)	# This Will Clear The Background Color To Black
+        glClearDepth(1.0)					# Enables Clearing Of The Depth Buffer
+        glDepthFunc(GL_LESS)				# The Type Of Depth Test To Do
+        glEnable(GL_DEPTH_TEST)				# Enables Depth Testing
+        glEnable(GL_TEXTURE_2D)
+        glShadeModel(GL_SMOOTH)				# Enables Smooth Color Shading
 	
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(45.0, float(w)/float(h), 0.1, 100.0)
+        glMatrixMode(GL_MODELVIEW)
+        
+    def resize(self, w, h):
+        if h == 0:
+	        h = 1
+        glViewport(0, 0, w, h)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(45.0, float(w)/float(h), 0.1, 100.0)
+        glMatrixMode(GL_MODELVIEW)
+    
+    def draw(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glLoadIdentity()
+        sc = self.grid_w / 8.0
+        dep = 8.0 * sc
+        c = sc / 2.0
+        
+        diff = time.time() - self.last_render
+        self.last_render = time.time()
+        
+        i = 0
+        for x in range(self.grid_w):
+            for y in range(self.grid_h):
+
+                stream = self.streams[i]
+                channel = stream.channel
+                if time.time() - channel.last_frame_time > 0.03:#channel.loop_delay:
+                    channel.last_frame_time = time.time()
+                    channel.frame_index += 1
+                    if channel.frame_index >= channel.frames:
+                        channel.frame_index = 0
+                    glDeleteTextures(channel.current_texture)
+                    channel.current_texture = None
+
+                if not channel.current_texture:
+                    channel.current_texture = channel.load_texture(channel.load_frame(channel.frame_index))
+                    
+                # Evenly spaced, centered grid
+                tx = -(self.grid_w-1) * sc / 2 + sc * x
+                ty = -(self.grid_h-1) * sc / 2 + sc * y
+                    
+                stream.tick(tx, ty, dep, diff)
+                
+                glBindTexture(GL_TEXTURE_2D, channel.current_texture)            
+                glBegin(GL_QUADS)
+                glTexCoord2f(0.0, 0.0)
+                glVertex3f(-c, -c, 0.0)
+                glTexCoord2f(1.0, 0.0)
+                glVertex3f(c, -c, 0.0)
+                glTexCoord2f(1.0, 1.0)
+                glVertex3f(c, c, 0.0)
+                glTexCoord2f(0.0, 1.0)
+                glVertex3f(-c, c, 0.0)
+                glEnd()
+                
+                stream.untick(tx, ty, dep, diff)
+                i += 1
+
+        glFinish()
+        glutSwapBuffers()
+    
+    def key_pressed(self, *args):
+        global full
+        # If escape is pressed, kill everything.
+        if args[0] == '\033':
+            glutDestroyWindow(self.window_id)
+            sys.exit()
+        elif args[0] == 'f':
+            full = not full
+            if full:
+                glutFullScreen(full)
+        elif args[0] == 's':
+            self.modes['spinning'] = not self.modes.get('spinning', False)
+        elif args[0] == 'z':
+            self.modes['zooming'] = not self.modes.get('zooming', False)
+	    
+    def idle(self):
+        glutPostRedisplay()
+
 # Print message to console, and kick off the main to get it rolling.
 print "Hit ESC key to quit."
 
 if __name__ == '__main__':
-	try:
-		GLU_VERSION_1_2
-	except:
-		print "Need GLU 1.2 to run this demo"
-		sys.exit(1)
-	main()
-	glutMainLoop()
+    grid = StreamGrid()
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
+    glutInitWindowSize(640, 480)
+    glutInitWindowPosition(0, 0)
+    window = glutCreateWindow("StreamGrid - \"Copyright violation from every angle!\"")
+    grid.window_id = window
+    glutDisplayFunc(grid.draw)
+    glutIdleFunc(grid.idle)
+    glutReshapeFunc(grid.resize)
+    glutKeyboardFunc(grid.key_pressed)
+
+    grid.init_gl(640, 480)
+    glutMainLoop()
     	
